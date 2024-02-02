cbb64a81fc78d0d2e260c3c2c6b22057b9944cd7
Joshua Lee <joshlee5@mit.edu>
2024-02-02 10:35:45 -0500

    fixed app.py file to allow for swagger ui

--- src/icatcher/icatcher_app/connexion_api/app.py
+++ src/icatcher/icatcher_app/connexion_api/app.py
@@ -1,17 +1,10 @@
 from connexion import FlaskApp
+from connexion.options import SwaggerUIOptions
 
-import app
+options = SwaggerUIOptions(swagger_ui_path="/docs")
 
-REACT_BUILD_FOLDER = str(
-    Path(Path(__file__).parent.parent, "frontend", "build").absolute()
-)
-REACT_APP_FILE = "index.html"
-
-app = FlaskApp(__name__, static_folder=REACT_BUILD_FOLDER)
-app.add_api("audited_labels_api.yaml")
-
-def run_app(port=5001, debug=False):
-    app.run(port=port, debug=debug)
+app = FlaskApp(__name__, specification_dir = ".", swagger_ui_options=options)
+app.add_api("audited_labels_api.yaml", swagger_ui_options=options)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001)
